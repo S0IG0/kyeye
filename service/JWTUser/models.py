ee0fe57293422ce0762127a87e02d807a28b79be
Daniil Chibitock <dchibitok@yandex.ru>
2023-03-06 01:53:44 +0300

    Написание новой модели User для работы с DRF

---
+++ service/JWTUser/models.py
@@ -0,0 +1,83 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin, UserManager, AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.core.mail import send_mail
+from django.core.validators import EmailValidator
+from django.db import models
+from django.utils import timezone
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+
+    username_validator = UnicodeUsernameValidator()
+    email_validator = EmailValidator()
+
+    username = models.CharField(
+        "username",
+        max_length=150,
+        unique=True,
+        help_text=
+        "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
+        ,
+        validators=[username_validator],
+        error_messages={
+            "unique": "A user with that username already exists.",
+        },
+    )
+    first_name = models.CharField("first name", max_length=150, )
+    last_name = models.CharField("last name", max_length=150, )
+    email = models.EmailField(
+        "email address",
+        unique=True,
+        validators=[email_validator],
+        error_messages={
+            "unique": "A user with that email address already exists.",
+        },
+    )
+    is_staff = models.BooleanField(
+        "staff status",
+        default=False,
+        help_text="Designates whether the user can log into this admin site.",
+    )
+    is_active = models.BooleanField(
+        "active",
+        default=True,
+        help_text=
+        "Designates whether this user should be treated as active. "
+        "Unselect this instead of deleting accounts."
+        ,
+    )
+    date_joined = models.DateTimeField("date joined", default=timezone.now)
+
+    objects = UserManager()
+
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    class Meta:
+        verbose_name = "user"
+        verbose_name_plural = "users"
+
+    def clean(self):
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
+
+    def get_full_name(self):
+        """
+        Return the first_name plus the last_name, with a space in between.
+        """
+        full_name = "%s %s" % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        """Return the short name for the user."""
+        return self.first_name
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """Send an email to this user."""
+        send_mail(subject, message, from_email, [self.email], **kwargs)
+
+    class Meta(AbstractUser.Meta):
+        swappable = "AUTH_USER_MODEL"
