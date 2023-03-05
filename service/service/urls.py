7bbbbbfec0678b701cb63d02d9a1a1c80f091532
Daniil Chibitock <dchibitok@yandex.ru>
2023-03-06 02:03:26 +0300

    Проброшены url. api/token/ - получения токена. api/token/refresh/ - обновления токена. api/token/verify/ - верификации токена. api/user/register/ - регистрация пользователя. api/user/update/<int:pk> - изменение данных конкретного пользователя.

--- service/service/urls.py
+++ service/service/urls.py
@@ -15,7 +15,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.routers import SimpleRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+from JWTUser.views import UserViewSet, UserRegisterView, UserUpdateView
+
+router = SimpleRouter()
+router.register(r'api/user', UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/user/register/', UserRegisterView.as_view(), name='register'),
+    path('api/user/update/<int:pk>', UserUpdateView.as_view(), name='update'),
 ]
+urlpatterns += router.urls
