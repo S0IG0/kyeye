6973fc8bda3760fadeab73a4b349197f9f455b03
Daniil Chibitock <dchibitok@yandex.ru>
2023-03-07 02:42:09 +0300

    создание регистрация моделей (Queue, UserQueueRelation) в админ панели.

--- service/Queue/admin.py
+++ service/Queue/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from Queue.models import Queue, UserQueueRelation
+
 # Register your models here.
+admin.site.register(Queue)
+admin.site.register(UserQueueRelation)
