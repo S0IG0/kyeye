baf7d68a5be4ec567649a9063a3258412d2eb008
Daniil Chibitock <dchibitok@yandex.ru>
2023-03-08 23:23:37 +0300

    Написано view, для удаление пользователя из очереди.

--- service/Queue/views.py
+++ service/Queue/views.py
@@ -1,6 +1,17 @@
 from rest_framework import status
-from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
-from rest_framework.permissions import IsAdminUser, IsAuthenticated, AllowAny
+from rest_framework.generics import (
+    CreateAPIView,
+    ListAPIView,
+    RetrieveAPIView,
+    DestroyAPIView
+)
+
+from rest_framework.permissions import (
+    IsAdminUser,
+    IsAuthenticated,
+    AllowAny
+)
+
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.settings import api_settings
@@ -14,13 +25,13 @@ from Queue.serializers import QueueSerializer, UserQueueRelationRegisterSerializ
 class QueueViewSet(ModelViewSet):
     queryset = Queue.objects.all()
     serializer_class = QueueSerializer
-    permission_classes = (IsAdminUser, )
+    permission_classes = (IsAdminUser,)
 
 
 class QueueRegisterView(CreateAPIView):
     queryset = Queue.objects.all()
     serializer_class = QueueSerializer
-    permission_classes = (IsAuthenticated, )
+    permission_classes = (IsAuthenticated,)
 
     def post(self, request: Request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
@@ -42,17 +53,36 @@ class QueueRegisterView(CreateAPIView):
         except (TypeError, KeyError):
             return {}
 
+
 class QueueListView(ListAPIView):
     queryset = Queue.objects.all()
     serializer_class = QueueSerializer
-    permission_classes = (AllowAny, )
+    permission_classes = (AllowAny,)
+
 
 class QueueRetrieveView(RetrieveAPIView):
     queryset = Queue.objects.all()
     serializer_class = QueueSerializer
-    permission_classes = (AllowAny, )
+    permission_classes = (AllowAny,)
+
 
 class UserQueueRelationRegisterView(CreateAPIView):
     queryset = UserQueueRelation.objects.all()
     serializer_class = UserQueueRelationRegisterSerializer
-    permission_classes = (AllowAny, )
+    permission_classes = (AllowAny,)
+
+
+class UserQueueRelationDestroyView(DestroyAPIView):
+    queryset = UserQueueRelation.objects.all()
+    serializer_class = UserQueueRelationRegisterSerializer
+    permission_classes = (AllowAny,)
+
+    def delete(self, request, *args, **kwargs):
+        user_id = kwargs.get('user_id')
+        queue_id = kwargs.get('queue_id')
+        try:
+            user_queue_relation = UserQueueRelation.objects.filter(user=user_id, queue=queue_id).last()
+            user_queue_relation.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except (UserQueueRelation.DoesNotExist, AttributeError):
+            return Response({'detail': 'UserQueueRelation not found'}, status=status.HTTP_404_NOT_FOUND)
