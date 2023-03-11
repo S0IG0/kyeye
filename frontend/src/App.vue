abe049b973ea53d1f540e24a051d7ae9cd84ef1e
Maxim <zhavoronkov03@inbox.ru>
2023-03-11 17:20:44 +0300

    displays main page using router-view

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="app">
-        <user-item v-for="user in users" :user="user"></user-item>
-    </div>
+  <div class="app">
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
@@ -11,45 +11,47 @@ import UserItem from "@/components/User/UserItem.vue"
 import MyButton from "@/components/UI/MyButton.vue";
 
 export default {
-    components: {
-      MyButton,
-        UserList, UserItem
-    }, 
-    data() {
-        return {
-            users: [],
-        }
+  components: {
+    MyButton,
+    UserList, UserItem
+  },
+  data() {
+    return {
+      users: [],
+    }
+  },
+  methods: {
+    async getAPIRequest() {
+      const x = await axios({
+        method: "get",
+        url: "http://localhost:8000/api/queue/",
+      });
+      // console.log(x.data[0].owner);
+      x.data[0].users.forEach(element => {
+        console.log(element.user);
+        this.users.push(element.user);
+      });
     },
-    methods: {
-        async getAPIRequest () {
-          const x = await axios({
-            method: "get",
-            url: "http://localhost:8000/api/queue/",
-          });
-          // console.log(x.data[0].owner);
-            x.data[0].users.forEach(element => {
-                console.log(element.user);
-                this.users.push(element.user);
-            });
-        },
-    },
-    mounted(){
-        this.getAPIRequest();
-    },
+  },
+  mounted() {
+    this.getAPIRequest();
+  },
 }
 </script>
 
 <style>
-    *{
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
-    form{
-        display: flex;
-        flex-direction: column;
-    }
-    .app{
-        padding: 30px;
-    }
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.app {
+  padding: 30px;
+}
 </style>
