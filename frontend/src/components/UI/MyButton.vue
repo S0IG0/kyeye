d8812636df320197c90cfbd21df72f1930a930ca
Maxim <zhavoronkov03@inbox.ru>
2023-03-11 13:57:31 +0300

    added myButton.vue (custom button)

---
+++ frontend/src/components/UI/MyButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <button class="btn">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+    export default { 
+        name: 'my-button'
+    }
+</script>
+
+<style scoped>
+    .btn{
+        padding: 10px 15px;
+        background: none;
+        color: gold;
+        border: 1px solid gold;
+    }
+
+</style>
