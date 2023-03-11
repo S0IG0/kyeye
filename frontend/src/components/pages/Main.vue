917a6d85e1daae86060524313c39b2e1295bd226
Maxim <zhavoronkov03@inbox.ru>
2023-03-11 17:19:43 +0300

    added main page

---
+++ frontend/src/components/pages/Main.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="main">
+    Добро пожаловать
+    <div class="buttons">
+      <my-button @click="$router.push('/register')">Регистрация</my-button>
+      <my-button @click="$router.push('/queue')">Очередь</my-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import MyButton from "@/components/UI/MyButton.vue";
+
+export default {
+  name: "Main",
+  components: {MyButton}
+}
+</script>
+
+<style scoped>
+  .main{
+    display: flex;
+    flex-direction: column;
+  }
+  my-button{
+    margin-top: 15px;
+    width: 100px;
+  }
+</style>
