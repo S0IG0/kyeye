3793a907621f026b3c5b6150057647ab7d981ba7
Maxim <zhavoronkov03@inbox.ru>
2023-03-11 15:57:35 +0300

    updated app.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,5 @@
 <template>
     <div class="app">
-        <!-- <post-form
-        @create="createPost"/>
-        <post-list :posts="posts"
-        @remove="removePost"
-        /> -->
-        <!-- <my-button style="margin-top: 15px" @click="getAPIRequest"> Get </my-button> -->
         <user-item v-for="user in users" :user="user"></user-item>
     </div>
 </template>
@@ -14,35 +8,25 @@
 import axios from 'axios'
 import UserList from "@/components/User/UserList.vue"
 import UserItem from "@/components/User/UserItem.vue"
+import MyButton from "@/components/UI/MyButton.vue";
 
 export default {
     components: {
+      MyButton,
         UserList, UserItem
     }, 
     data() {
         return {
-            posts: [
-                {id: 1, title: "JavaScript", body: "Описание"},
-                {id: 2, title: "JavaScript 2", body: "Описание 2"},
-                {id: 3, title: "JavaScript 3", body: "Описание 3"},
-                {id: 4, title: "JavaScript 4", body: "Описание 4"},
-            ],
             users: [],
         }
     },
     methods: {
-        createPost(post) {
-            this.posts.push(post);
-        },
-        removePost(post){
-            this.posts = this.posts.filter(p => p.id !== post.id);
-        },
         async getAPIRequest () {
-            var x = await axios({
-                method: "get",
-                url: "http://localhost:8000/api/queue/",
-            })
-            // console.log(x.data[0].owner);
+          const x = await axios({
+            method: "get",
+            url: "http://localhost:8000/api/queue/",
+          });
+          // console.log(x.data[0].owner);
             x.data[0].users.forEach(element => {
                 console.log(element.user);
                 this.users.push(element.user);
