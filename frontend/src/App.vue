645a70b036cf4c7d92a67f7726ab7c24c07a4849
Daniil Chibitock <dchibitok@yandex.ru>
2023-03-12 18:05:17 +0300

    -

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="app">
-        <user-item v-for="user in users" :user="user"></user-item>
-    </div>
+  <div class="app">
+    <user-item v-for="user in users" :user="user"></user-item>
+  </div>
 </template>
 
 <script>
@@ -11,45 +11,49 @@ import UserItem from "@/components/User/UserItem.vue"
 import MyButton from "@/components/UI/MyButton.vue";
 
 export default {
-    components: {
-      MyButton,
-        UserList, UserItem
-    }, 
-    data() {
-        return {
-            users: [],
-        }
+  components: {
+    MyButton,
+    UserList, UserItem
+  },
+  data() {
+    return {
+      users: [],
+      // backend_ip: '192.168.1.64', // localhost
+      backend_ip: 'localhost', // localhost
+    }
+  },
+  methods: {
+    async getAPIRequest() {
+      const x = await axios({
+        method: "get",
+        url: `http://${this.backend_ip}:8000/api/queue/`,
+      });
+      x.data[0].users.forEach(element => {
+        console.log(element.user);
+        this.users.push(element.user);
+      });
     },
-    methods: {
-        async getAPIRequest () {
-          const x = await axios({
-            method: "get",
-            url: "http://localhost:8000/api/queue/",
-          });
-          // console.log(x.data[0].owner);
-            x.data[0].users.forEach(element => {
-                console.log(element.user);
-                this.users.push(element.user);
-            });
-        },
-    },
-    mounted(){
-        this.getAPIRequest();
-    },
+  },
+
+  mounted() {
+    this.getAPIRequest();
+  },
 }
 </script>
 
 <style>
-    *{
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
-    form{
-        display: flex;
-        flex-direction: column;
-    }
-    .app{
-        padding: 30px;
-    }
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.app {
+  padding: 30px;
+}
 </style>
--- service/service/settings.py
+++ service/service/settings.py
@@ -18,7 +18,6 @@ from django.conf import settings
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -28,8 +27,10 @@ SECRET_KEY = 'django-insecure-t8u$3e2_n%kzc!v3q9vnrdbu_)w#a=$l_2+2mn(w#uo@vlezn=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    # '192.168.1.64',
+    # 'localhost',
+]
 
 # Application definition
 
@@ -79,7 +80,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'service.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -111,7 +111,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -122,7 +121,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
