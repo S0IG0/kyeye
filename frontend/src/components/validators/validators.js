fbaef5b668d6934bfc0c44350209e02eb50c1083
Daniil Chibitock <dchibitok@yandex.ru>
2023-03-14 00:57:26 +0300

    added new validators

--- frontend/src/components/validators/validators.js
+++ frontend/src/components/validators/validators.js
@@ -1,7 +1,34 @@
-function validatePassword(password, repeat_password){
-    if (password !== repeat_password){
-        return ["Invalid password"];
+export function validatePassword(password, repeat_password) {
+    let errors = []
+    if (password !== repeat_password) {
+        errors.push("Invalid password");
     }
-    return [];
+    if (!String(password).match(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/)) {
+        errors.push("The password is too simple");
+    }
+    if (password === '') {
+        errors.push("Password is require");
+    }
+
+    return errors;
 }
-export default validatePassword
+
+export function validateEmail(email) {
+    let errors = []
+    if (!String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        )) {
+        errors.push("Invalid email");
+    }
+    return errors;
+}
+
+export function validateName(type, name) {
+    let errors = []
+    if (name === '') {
+        errors.push(`${type} name is require`);
+    }
+    return errors;
+}
