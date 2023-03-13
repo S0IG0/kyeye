242bcdb0bdefeea372887d4c79f3c57f0e79b2eb
Daniil Chibitock <dchibitok@yandex.ru>
2023-03-13 23:20:35 +0300

    delete console.log

--- frontend/src/components/validators/validators.js
+++ frontend/src/components/validators/validators.js
@@ -1,7 +1,5 @@
 function validatePassword(password, repeat_password){
     if (password !== repeat_password){
-        console.log(password)
-        console.log(repeat_password)
         return ["Invalid password"];
     }
     return [];
