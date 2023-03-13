9716e6e3b8c373756d4dd681444a5d8b9a544b02
Maxim <zhavoronkov03@inbox.ru>
2023-03-13 23:05:47 +0300

    Added errors and errorlists

--- frontend/src/components/UI/index.js
+++ frontend/src/components/UI/index.js
@@ -1,7 +1,11 @@
 import MyButton from "@/components/UI/MyButton";
 import MyInput from "@/components/UI/MyInput";
+import Error from "@/components/UX/Error.vue";
+import ErrorList from "@/components/UX/ErrorList.vue";
 
 export default [
     MyButton, 
-    MyInput
+    MyInput,
+    Error,
+    ErrorList,
 ]
