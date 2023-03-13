8b3598ef76953d733759e9a7fc36a0c0851ffc9a
Maxim <zhavoronkov03@inbox.ru>
2023-03-13 23:07:35 +0300

    added error and errorlist components

---
+++ frontend/src/components/UX/ErrorList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="error-list"
+  v-for="error in errors"
+  :key="error">
+    <error>{{ error }}</error>
+  </div>
+</template>
+
+<script>
+import Error from "@/components/UX/Error.vue";
+
+export default {
+  name: "error-list",
+  components: {Error},
+  props:{
+    errors: {
+      type: Array,
+    }
+  },
+
+}
+</script>
+
+<style scoped>
+  .error-list{
+    width: 500px;
+    height: 200px;
+    border: 2px solid red;
+    margin-top: 15px;
+  }
+
+</style>
