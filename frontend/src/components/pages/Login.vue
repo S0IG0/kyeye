d72cc7f61d124142d13418965908c70c3bd57fa8
Maxim <zhavoronkov03@inbox.ru>
2023-03-14 17:42:55 +0300

    added login page

---
+++ frontend/src/components/pages/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <login-form></login-form>
+</template>
+
+<script>
+import LoginForm from "@/components/UI/LoginForm.vue";
+
+export default {
+  name: "Login",
+  components: {LoginForm}
+}
+</script>
+
+<style scoped>
+
+</style>
