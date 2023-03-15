e502c71f288e44589d9d5572fa2c42d5f1098892
Даниил <dchibitok@yandex.ru>
2023-03-15 19:37:08 +0300

    create JwtToken class

---
+++ frontend/src/components/js/JwtToken.js
@@ -0,0 +1,63 @@
+import axios from "axios";
+import {specialWord} from "@/components/config";
+
+export class JwtToken {
+    constructor(access, refresh) {
+        if (access === undefined || refresh === undefined) {
+            this.loadToLocalStorage();
+        } else {
+            this.access = access;
+            this.refresh = refresh;
+            this.saveToLocalStorage();
+        }
+    }
+
+    loadToLocalStorage() {
+        const data = JSON.parse(localStorage.getItem('JwtToken'));
+        this.access = data.access;
+        this.refresh = data.refresh;
+    }
+
+    saveToLocalStorage() {
+        localStorage.setItem('JwtToken', JSON.stringify(this));
+    }
+
+    removeToLocalStorage() {
+        localStorage.removeItem('JwtToken');
+    }
+
+    async checkIsActiveToken(token) {
+        let is_active = false;
+        await axios({
+            method: "post",
+            url: "http://localhost:8000/api/token/verify/",
+            data: {
+                'token': token,
+            },
+        }).then(() => {
+            is_active = true;
+        })
+
+        return is_active;
+    }
+
+
+    async refreshAccess() {
+        await axios({
+            method: 'post',
+            url: 'http://localhost:8000/api/token/refresh/',
+            data: {
+                'refresh': this.refresh,
+            }
+        }).then(response => {
+            this.access = response.data.access;
+            this.saveToLocalStorage();
+        }).catch(error => {
+            console.log('refreshAccess.catch', error)
+        })
+    }
+
+    getAuthorization() {
+        return `${specialWord}} ${this.access}`
+    }
+}
