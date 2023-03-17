266a0b7f92a5e890f5ebe8b6b4ad4e73649a85c6
Daniil Chibitock <dchibitok@yandex.ru>
2023-03-18 01:37:17 +0300

    added new permission IsOwnerQueue

--- backend/service/JWTUser/permissions.py
+++ backend/service/JWTUser/permissions.py
@@ -9,3 +9,14 @@ class IsOwner(BasePermission):
             request.user.is_authenticated and
             request.user.id == view.kwargs.get('pk', -1)
         )
+
+
+class IsOwnerQueue(BasePermission):
+    """
+    Custom permission to only allow owners of an object to delete it.
+    """
+    message = 'You must be the owner of this queue to delete a subscriber.'
+
+    def has_object_permission(self, request, view, obj):
+        # Проверяем, является ли текущий пользователь владельцем очереди, к которой относится удаляемый объект
+        return request.user.id == obj.owner.id
