525a12d03511af2ddb779fbbc3e419f0086b20a1
Daniil Chibitock <dchibitok@yandex.ru>
2023-03-20 01:07:56 +0300

    файл с настройкой celery

---
+++ backend/service/celery_app.py
@@ -0,0 +1,14 @@
+import os
+
+from celery import Celery
+from django.conf import settings
+
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'service.settings'
+)
+
+app = Celery('service')
+app.config_from_object('django.conf:settings')
+app.conf.broker_url = settings.CELERY_BROKER_URL
+app.autodiscover_tasks()
