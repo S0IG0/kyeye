f69b8384d63a6a83f03ce0ffb3709bdaf627b484
Daniil Chibitock <dchibitok@yandex.ru>
2023-03-20 01:07:06 +0300

    добавлен метод ready который импортирует сигналы

--- backend/service/Queue/apps.py
+++ backend/service/Queue/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class QueueConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Queue'
+
+    def ready(self):
+        import Queue.signals
