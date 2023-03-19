d7eafc0343299a89e90fe04e8bf35cc2f9554f52
Daniil Chibitock <dchibitok@yandex.ru>
2023-03-20 01:14:29 +0300

    Добавлена функция get_queue для получения определенной очереди. Если не сможем получить очередь возвращаем None

---
+++ backend/service/Queue/services.py
@@ -0,0 +1,11 @@
+from Queue.models import Queue
+
+
+def get_queue(pk: int) -> Queue | None:
+    queue = None
+    try:
+        queue = Queue.objects.get(pk=pk)
+    except Queue.DoesNotExist as exception:
+        print(exception)
+
+    return queue
