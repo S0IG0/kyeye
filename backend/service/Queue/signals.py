a35dfaa2573a68ca220bed690420511bf25c5aba
Даниил <dchibitok@yandex.ru>
2023-03-20 15:19:22 +0300

    fix

--- backend/service/Queue/signals.py
+++ backend/service/Queue/signals.py
@@ -3,9 +3,21 @@ from django.dispatch import receiver
 
 from Queue.models import Queue
 from Queue.tasks import activate_queue
-
+from Queue.services import get_queue
+from datetime import datetime, timedelta
 
 @receiver(post_save, sender=Queue)
 def update_stock(sender, instance: Queue, created, **kwargs):
     if created:
-        activate_queue.delay(instance.pk)
+        queue_id = instance.pk
+        queue = get_queue(queue_id)
+        if (queue is not None) and (queue.is_active == False):
+            result: timedelta = queue.date_activation - queue.date_creation
+            seconds = result.total_seconds()
+            if seconds > 0:
+#                 activate_queue.apply_async((queue_id, ), etc=queue.date_activation)
+#                 activate_queue.apply_async((queue_id, ), etc=datetime.utcnow() + timedelta(seconds=120))
+                activate_queue.apply_async((queue_id, ), countdown=seconds)
+            else:
+                queue.is_active = True
+                queue.save()
