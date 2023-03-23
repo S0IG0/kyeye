b6edc9fd7d3aef920f3fa64539939658c6c619f9
Daniil Chibitock <dchibitok@yandex.ru>
2023-03-24 02:54:52 +0300

    added view for queues

--- frontend/src/components/pages/Queue.vue
+++ frontend/src/components/pages/Queue.vue
@@ -1,15 +1,29 @@
 <template>
   <div class="queue">
-    <user-item v-for="user in users" :user="user"></user-item>
+<!--    <user-item v-for="user in users" :user="user"></user-item>-->
+    <div v-if="queues">
+      <div v-for="item in queues['edges']">
+        <div class="queue_item">
+          name: {{ item['node']['name'] }} |
+          dateActivation: {{ item['node']['dateActivation'] }} |
+          dateCreation: {{ item['node']['dateCreation'] }}
+          <div class="users">
+            <div class="user_item" v-for="user_node in item['node']['users']['edges']">
+              email: {{ user_node['node']['user']['email'] }} |
+              username: {{ user_node['node']['user']['username'] }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
 import UserList from "@/components/User/UserList.vue"
 import UserItem from "@/components/User/UserItem.vue"
 import MyButton from "@/components/UI/MyButton.vue";
-import {urlBackend} from "@/components/config";
+import gql from "graphql-tag";
 
 export default {
   components: {
@@ -18,24 +32,13 @@ export default {
   },
   data() {
     return {
-      users: [],
-    }
+      queues: null,
+    };
   },
-  methods: {
-    async getAPIRequest() {
-      const x = await axios({
-        method: "get",
-        url: `${urlBackend}/api/queue/`,
-      });
-      // console.log(x.data[0].owner);
-      x.data[0].users.forEach(element => {
-        console.log(element.user);
-        this.users.push(element.user);
-      });
+  apollo: {
+    queues: {
+      query: gql`query{queues (first: 10, ) {edges {node {id, name, dateActivation, dateCreation, users{edges{node{id, user{email, username}}}}}}}}`,
     },
-  },
-  mounted() {
-    this.getAPIRequest();
   },
 }
 </script>
@@ -44,5 +47,29 @@ export default {
 form {
   display: flex;
   flex-direction: column;
+}
+
+.queue_item {
+  margin: 15px;
+  padding: 15px;
+  background-color: whitesmoke;
+  border: black 2px solid;
+  border-radius: 15px;
+  font-size: 20px;
+}
+
+.users {
+  margin: 20px;
+  border: black 2px solid;
+  border-radius: 15px;
+}
+
+.user_item {
+  margin: 15px;
+  padding: 15px;
+  background-color: dodgerblue;
+  border-radius: 15px;
+  color: white;
+  font-size: 20px;
 }
 </style>
