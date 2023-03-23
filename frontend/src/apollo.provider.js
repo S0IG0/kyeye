f2b10c72762e91ebacbfc0400689b8334947c23d
Daniil Chibitock <dchibitok@yandex.ru>
2023-03-24 02:53:29 +0300

    configuration file for apollo

---
+++ frontend/src/apollo.provider.js
@@ -0,0 +1,40 @@
+import {InMemoryCache, ApolloClient, ApolloLink, HttpLink} from "@apollo/client";
+import {createApolloProvider} from "@vue/apollo-option";
+import {setContext} from "@apollo/client/link/context";
+import {Auth} from "@/components/js/AuthModule";
+
+let token;
+const contextLink = setContext(async () => {
+    token = await Auth.getAuthorization()
+    return {token}
+})
+
+const httpLink = new HttpLink({
+    uri: "http://localhost:8000/graphql",
+})
+
+const authLink = new ApolloLink((operation, forward) => {
+    const {token} = operation.getContext();
+    if (token) {
+        operation.setContext(({headers = {}}) => ({
+            headers: {
+                ...headers,
+                Authorization: token,
+            },
+        }));
+    }
+
+    return forward(operation);
+});
+
+const link = ApolloLink.from([contextLink, authLink.concat(httpLink)]);
+const cache = new InMemoryCache();
+
+const apolloClient = new ApolloClient({
+    cache,
+    link,
+});
+
+export const provider = createApolloProvider({
+    defaultClient: apolloClient,
+});
