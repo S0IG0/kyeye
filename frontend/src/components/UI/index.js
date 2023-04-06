9a3b0e3559e02fffed8ade8a4075c652067f2871
Maxim <zhavoronkov03@inbox.ru>
2023-04-06 23:43:12 +0300

    Added sidebar menu component

--- frontend/src/components/UI/index.js
+++ frontend/src/components/UI/index.js
@@ -3,6 +3,7 @@ import MyInput from "@/components/UI/MyInput";
 import Error from "@/components/UX/Error.vue";
 import ErrorList from "@/components/UX/ErrorList.vue";
 import ModalWindow from "@/components/UI/ModalWindow.vue";
+import SidebarMenu from "@/components/UI/PersonalAccount/SidebarMenu.vue";
 
 export default [
     MyButton, 
@@ -10,4 +11,5 @@ export default [
     Error,
     ErrorList,
     ModalWindow,
+    SidebarMenu,
 ]
